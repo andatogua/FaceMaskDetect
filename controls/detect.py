389d38cb6d1437d0be46cd08e77ba8a764cabae5
andatogua <68097749+andatogua@users.noreply.github.com>
2021-08-26 10:45:21 -0500

    Update detect.py

--- controls/detect.py
+++ controls/detect.py
@@ -6,9 +6,7 @@ from PyQt5.QtCore import *
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
 import numpy as np
-import imutils
 import time
 import cv2
 import os
